cfab4ab2b9075967a12312030e7a6d5bcac0bd2d
Soh Jun Wen <mail.junwen@gmail.com>
2025-04-07 12:02:14 +0800

    Add monochrome color palette

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  black: "hsl(0, 0%, 0%)",
+  neutral1: "hsl(213, 26%, 23%)",
+  neutral2: "hsl(213, 26%, 23%)",
+  neutral3: "hsl(214, 11%, 37%)",
+  neutral4: "hsl(215, 8%, 46%)",
+  neutral5: "hsl(212, 7%, 55%)",
+  neutral6: "hsl(214, 8%, 67%)",
+  neutral7: "hsl(213, 14%, 87%)",
+  neutral8: "hsl(214, 21%, 94%)",
+  neutral9: "hsl(216, 29%, 97%)",
+  white: "hsl(0, 0%, 100%)",
+};
